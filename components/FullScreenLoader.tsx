e3b6bb7f9147f2265f26230e757cdb6bfe785ce5
sudinamproducts <sudinamproducts@gmail.com>
2026-02-01 13:01:37 +0400

    added mobile video

--- components/FullScreenLoader.tsx
+++ components/FullScreenLoader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function FullScreenLoader({
@@ -10,30 +10,44 @@ export default function FullScreenLoader({
 }) {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2600); // or use onEnded
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
       <AnimatePresence>
         {loading && (
           <motion.div
-            className="fixed inset-0 z-[9999] bg-black"
+            className="fixed inset-0 z-[9999] bg-black overflow-hidden"
             initial={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.9 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
           >
+            {/* DESKTOP VIDEO */}
             <video
-              src="/logo-loader.mp4"
+              src="/brand/logo-loader-desktop.mp4"
               autoPlay
               muted
               playsInline
-              className="w-full h-full object-cover"
+              onEnded={() => setLoading(false)}
+              className="
+                hidden md:block
+                absolute inset-0
+                w-full h-full
+                object-cover
+              "
+            />
+
+            {/* MOBILE VIDEO */}
+            <video
+              src="/brand/logo-loader-mobile.mp4"
+              autoPlay
+              muted
+              playsInline
+              onEnded={() => setLoading(false)}
+              className="
+                block md:hidden
+                absolute inset-0
+                w-full h-full
+                object-cover
+              "
             />
           </motion.div>
         )}
